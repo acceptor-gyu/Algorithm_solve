1b74b48b91539c0388efb15ed774f7421b90d467
acceptor-gyu <gseonggyu968@gmail.com>
2023-02-19 10:47:14 +0900

    solve: [acmicpc.net 1620] using HashMap when check containsKey and using array when inqure value by index

--- acmicpc.net/나는야_포켓몬_마스터_이다솜_1620.java
+++ acmicpc.net/나는야_포켓몬_마스터_이다솜_1620.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-public class 나는야_포켓몬_마스터_이다솜 {
+public class 나는야_포켓몬_마스터_이다솜_1620 {
 
     public static void main(String[] args) throws IOException {
 
@@ -16,25 +16,26 @@ public class 나는야_포켓몬_마스터_이다솜 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Map<String, String> pocketMonsters = new HashMap<String, String>();
+        String[] key = new String[N + 1];
+        int[] value = new int[N + 1];
+        Map<String, Integer> pocketMonsters = new HashMap<String, Integer>();
 
         for (int i = 1; i <= N; i++) {
-            pocketMonsters.put(br.readLine(), String.valueOf(i));
+            key[i] = br.readLine();
+            value[i] = i;
+            pocketMonsters.put(key[i], i);
         }
 
         for (int i = 0; i < M; i++) {
-            String value = br.readLine();
-
-            if (pocketMonsters.containsKey(value)) {
-                System.out.println(pocketMonsters.get(value));
+            String whatNum = br.readLine();
+            if (pocketMonsters.containsKey(whatNum)) {
+                System.out.println(pocketMonsters.get(whatNum));
                 continue;
             }
 
-            for (Map.Entry<String, String> entry : pocketMonsters.entrySet()) {
-                if (entry.getValue().equals(value)) {
-                    System.out.println(entry.getKey());
-                }
-            }
+            int whatMonster = Integer.parseInt(whatNum);
+
+            System.out.println(key[whatMonster]);
         }
     }
 }
