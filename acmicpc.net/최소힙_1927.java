5afb0027ebc0ef0c45318cfe682ec9c4327ba160
acceptor-gyu <gseonggyu968@gmail.com>
2023-02-21 20:44:26 +0900

    solve: [acmicpc.net 1927] using priority queue

---
+++ acmicpc.net/최소힙_1927.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class 최소힙_1927 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                if (pq.isEmpty()) {
+                    sb.append("0\n");
+                    continue;
+                }
+                sb.append(pq.poll()).append("\n");
+                continue;
+            }
+            pq.add(num);
+        }
+        System.out.println(sb);
+    }
+}
