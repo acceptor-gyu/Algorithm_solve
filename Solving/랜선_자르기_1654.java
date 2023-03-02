0d1ccd2468c72a43eac109ec94e8161aed00320a
acceptor-gyu <gseonggyu968@gmail.com>
2023-03-02 18:19:23 +0900

    solve: [acpcmic.net 17298] 스택 활용

---
+++ Solving/랜선_자르기_1654.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ *
+ *
+ * Binary Search / Parametric Search
+ *
+ */
+
+public class 랜선_자르기_1654 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+
+    }
+}
---
+++ acmicpc.net/오큰수_17298.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class 오큰수_17298 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] num = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        int[] answer = new int[N];
+
+        for (int i = N - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peekLast() <= num[i]) {
+                stack.pollLast();
+            }
+
+            answer[i] = stack.isEmpty() ? -1 : stack.peekLast();
+
+            stack.addLast(num[i]);
+        }
+
+        for (int i : answer) {
+            bw.write(i + " ");
+        }
+
+        bw.flush();
+    }
+}
