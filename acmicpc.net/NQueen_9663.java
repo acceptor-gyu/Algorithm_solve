5a89c3f1d942758fa2d178d5e5d4e946d2371c13
acceptor-gyu <gseonggyu968@gmail.com>
2023-08-03 15:40:05 +0900

    solve: [acmicpc.net 9663] NQueen 백트랙킹

--- acmicpc.net/NQueen_9663.java
+++ acmicpc.net/NQueen_9663.java
@@ -5,21 +5,21 @@ import java.io.InputStreamReader;
 public class NQueen_9663 {
 
     private static int N;
-    private static boolean[][] location;
+    private static int[] location;
     private static int count = 0;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        location = new boolean[N][N];
+        location = new int[N];
 
-        backtrack(0, 0, 0);
+        backtrack(0);
 
         System.out.println(count);
     }
 
-    private static void backtrack(int r, int c, int placed) {
+    private static void backtrack(int placed) {
 
         if (placed == N) {
             count++;
@@ -27,69 +27,25 @@ public class NQueen_9663 {
         }
 
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (!location[i][j]) {
-                    unableToPlace(i, j);
-                    backtrack(i, j, placed + 1);
-                    ableToPlace(i, j);
-                }
+            location[placed] = i;
+
+            if (placeable(placed)) {
+                backtrack(placed + 1);
             }
         }
     }
 
-    private static void unableToPlace(int r, int c) {
+    private static boolean placeable(int placed) {
 
-        for (int i = 0; i < N; i++) {
-            location[r][i] = true;
-            location[i][c] = true;
-        }
-
-        int nextR = r;
-        int nextC = c;
-
-        while (true) {
-            if ((nextR < 0 || nextC < 0 || nextR >= N || nextC >= N)) {
-                break;
+        for (int i = 0; i < placed; i++) {
+            if (location[placed] == location[i]) {
+                return false;
             }
-            location[nextR++][nextC++] = true;
-        }
-
-        nextR = r;
-        nextC = c;
-
-        while (true) {
-            if ((nextR < 0 || nextC < 0 || nextR >= N || nextC >= N)) {
-                break;
+            if (Math.abs(placed - i) == Math.abs(location[placed] - location[i])) {
+                return false;
             }
-            location[nextR++][nextC--] = true;
-        }
-    }
-
-    private static void ableToPlace(int r, int c) {
-
-        for (int i = 0; i < N; i++) {
-            location[r][i] = false;
-            location[i][c] = false;
         }
 
-        int nextR = r;
-        int nextC = c;
-
-        while (true) {
-            if ((nextR < 0 || nextC < 0 || nextR >= N || nextC >= N)) {
-                break;
-            }
-            location[nextR++][nextC++] = false;
-        }
-
-        nextR = r;
-        nextC = c;
-
-        while (true) {
-            if ((nextR < 0 || nextC < 0 || nextR >= N || nextC >= N)) {
-                break;
-            }
-            location[nextR++][nextC--] = false;
-        }
+        return true;
     }
 }
