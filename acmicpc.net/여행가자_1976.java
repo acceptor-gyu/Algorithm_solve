d6400cd3329ca0af92ba8067e941c205969fab1e
luke <gseonggyu968@gmail.com>
2024-01-31 00:44:49 +0900

    solve: [acmicpc.net1976] union find

--- acmicpc.net/여행가자_1976.java
+++ acmicpc.net/여행가자_1976.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class 여행가자_1976 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 여행가자_1976 {
+
+    private static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 1; i <= N; i++) {
+
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+
+                int temp = Integer.parseInt(st.nextToken());
+                if (temp == 1) {
+                    union(i, j);
+                }
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = find(Integer.parseInt(st.nextToken()));
+        for (int i = 1; i < M; i++) {
+            int city = Integer.parseInt(st.nextToken());
+
+            if (start != find(city)) {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
+    }
+
+    private static void union(int a, int b) {
+        int x = find(a);
+        int y = find(b);
+        parent[x] = y;
+    }
+
+    private static int find(int a) {
+        if (parent[a] == a)
+            return a;
+        return parent[a] = find(parent[a]);
+    }
 }
