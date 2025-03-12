7c04a0079bb2ed0d505c813db0472952074e4b37
luke <gseonggyu968@gmail.com>
2025-03-12 17:04:13 +0900

    boj[2143] 누적합(PrefixSum) Map 활용
    연속된 배열은 누적합을 고려하자

--- acmicpc.net/prefix_sum/두배열의합_2143.java
+++ acmicpc.net/prefix_sum/두배열의합_2143.java
@@ -1,4 +1,61 @@
 package prefix_sum;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 public class 두배열의합_2143 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] A = new int[n];
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int[] B = new int[m];
+        for (int i = 0; i < m; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Map<Integer, Integer> prefixMapOfA = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += A[j];
+                prefixMapOfA.put(sum, prefixMapOfA.getOrDefault(sum, 0) + 1);
+            }
+        }
+
+        Map<Integer, Integer> prefixMapOfB = new HashMap<>();
+        for (int i = 0; i < m; i++) {
+            int sum = 0;
+            for (int j = i; j < m; j++) {
+                sum += B[j];
+                prefixMapOfB.put(sum, prefixMapOfB.getOrDefault(sum, 0) + 1);
+            }
+        }
+
+        long result = 0;
+
+        for (Integer keyA : prefixMapOfA.keySet()) {
+            if (prefixMapOfB.containsKey((T - keyA))) {
+                result += (long) prefixMapOfA.get(keyA) * prefixMapOfB.get(T - keyA);
+            }
+        }
+
+        System.out.println(result);
+    }
+
 }
