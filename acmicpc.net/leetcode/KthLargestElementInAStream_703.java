0ac4e52cddb1db5dc165e379b31238716f8f72cf
luke <gseonggyu968@gmail.com>
2026-01-01 23:29:26 +0900

    priority queue

--- acmicpc.net/leetcode/KthLargestElementInAStream_703.java
+++ acmicpc.net/leetcode/KthLargestElementInAStream_703.java
@@ -9,17 +9,25 @@ public class KthLargestElementInAStream_703 {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         public KthLargest(int k, int[] nums) {
+
             this.k = k;
 
-            for(int num : nums) pq.add(num);
+            for(int num : nums) {
+                pq.add(num);
+            }
 
-            while(pq.size() > k) pq.poll();
+            while(pq.size() > k) {
+                pq.poll();
+            }
         }
 
         public int add(int val) {
+
             pq.add(val);
 
-            if(pq.size() > k) pq.poll();
+            if(pq.size() > k) {
+                pq.poll();
+            }
 
             return pq.peek();
         }
